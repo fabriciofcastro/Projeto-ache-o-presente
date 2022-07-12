e3dbe03095c3da8a096395ac8feb3f63646cbbdc
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2022-07-12 17:40:35 -0300

    Aprimorando estilo do componente porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Porta from '../components/porta'
 import Presente from '../components/Presente'
+import styles from '../styles/divGlobal.module.css'
 
 
 export default function Home() {
   return (
-    <div >
-     <Presente></Presente>
-     <Porta />
+    <div className={styles.div_global}>
+      <Presente></Presente>
+      <Porta /> 
+      <Porta />
     </div>
   )
 }
