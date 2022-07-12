f585cee06591335f613d33edf25b19a6bab2af83
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2022-07-12 15:02:16 -0300

    criado componetes para ser o presente.jsx, juntamente com presente.moudle.css

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Presente from '../components/Presente'
+
+
+export default function Home() {
+  return (
+    <div >
+     <Presente></Presente>
+    </div>
+  )
+}
