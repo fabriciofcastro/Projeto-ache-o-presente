1ac00e3bb9a84c8be882d247de3fabfbece3599a
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2022-07-12 17:30:58 -0300

    refinamento do componente porta com css

--- components/porta.jsx
+++ components/porta.jsx
@@ -3,8 +3,16 @@ import styles from '../styles/porta.module.css'
 export default function Porta(props) {
     
     return (
-        <div className={styles.porta}>
-            <div className={styles.numero}> 3 </div>
+        
+        <div className={styles.area}>
+            <div className={styles.estrutura}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}> 3 </div>
+                </div>
+            </div>
+            
         </div>
+        
+       
     )
 }
